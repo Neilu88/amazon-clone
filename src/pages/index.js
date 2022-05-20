23eb16ef06df7e633d5e65c78c455ccdbb28bea7
Neil <neildatta8888@gmail.com>
2022-05-20 18:33:49 -0400

    Finished Slides component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import Head from "next/head"
 import Header from "../components/Header.js"
+import Slides from "../components/Slides.js"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
         <link rel="shortcut icon" href="./favicon.ico" />
       </Head>
 
       <Header />
-      {/* <Slides /> */}
-      {/* <Feed /> */}
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Slides />
+        {/* <Feed /> */}
+      </main>
     </div>
   )
 }

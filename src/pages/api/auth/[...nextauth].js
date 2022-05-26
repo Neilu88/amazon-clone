494ae2b113356221e303fa48596ed69046b2a47e
Neil <neildatta8888@gmail.com>
2022-05-25 21:30:04 -0400

    Added secret variable

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
   // Configure one or more authentication providers
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
